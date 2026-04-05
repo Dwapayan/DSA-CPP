e303186ee39858195a6b812ca85d8d45d1aa5ccb
Dwapayan <dwapayan@gmail.com>
2026-04-05 21:30:36 +0530

    updated bin to decimal

--- decimalTobinary.cpp
+++ decimalTobinary.cpp
@@ -15,6 +15,18 @@ int toBinary(int n){
     return binaryNumber;
 }
 
+int toDecimal(int binaryNumber){
+    int decimalNumber=0;
+    int power=1;
+
+    while(binaryNumber > 0){
+        decimalNumber+= binaryNumber%10*power;
+        binaryNumber/=10;
+        power*=2;
+    }
+    return decimalNumber;
+}
+
 void printBinaryRange(int rangeN){
     for (int i=1;i<=rangeN;i++){
         cout << toBinary(i) << endl;
@@ -23,11 +35,13 @@ void printBinaryRange(int rangeN){
 
 int main(){
     int n, rangeN;
+    int binNumber=1100101;
     cout << "Enter decimal number for binary conversion :" ; 
     cin >> n;
     cout << "Binay number representing " << n << " is " << toBinary(n) <<endl;
     cout << "Enter number range to print binary numbers :" ; 
     cin >> rangeN;
     printBinaryRange(rangeN);
+    cout << "Decimal number representing " << binNumber << " is " << toDecimal(binNumber) <<endl;
     return 0;
 }
